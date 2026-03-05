980fcbdc2ad5bc3559f3832eb6373536a67e93b6
mariapla <54579606+mariapla@users.noreply.github.com>
2026-03-05 18:23:39 +0100

    Add loading animation script in scripts.js

---
+++ ejercicios/carga/scripts.js
@@ -0,0 +1,23 @@
+
+const texto = document.querySelector('.carga')
+const fondo = document.querySelector('.fondo')
+
+let carga = 0
+
+let frecuencia = setInterval(desenfoque, 30)
+
+function desenfoque(){
+    carga++
+
+    if(carga > 99){
+        clearInterval(frecuencia)
+    }
+
+    texto.innerText = `${carga}%`
+    texto.style.opacity = mapeo(carga, 0, 100, 1, 0)
+    fondo.style.filter = `blur(${mapeo(carga, 0, 100, 30, 0)}px)`
+}
+
+const mapeo = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
