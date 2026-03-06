46af443bb73859f0320282ea2748c12684b28630
mariapla <54579606+mariapla@users.noreply.github.com>
2026-03-06 10:57:46 +0100

    Update hexa.js

--- ejercicios/fondoAleatorio/hexa.js
+++ ejercicios/fondoAleatorio/hexa.js
@@ -1,8 +1,8 @@
 const hexa = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
 
-const boton = document.getElementById('hexa')
+const botonHexa = document.getElementById('hexa')
 
-boton.addEventListener('click', function(){
+botonHexa.addEventListener('click', function(){
     let color = "#"
     for(let i = 0; i < 6; i++){
         color += hexa[Math.floor(Math.random()*hexa.length+1)]
