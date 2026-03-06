b30760ed2788c1355b61545c85c23c5bfbe73ef4
mariapla <54579606+mariapla@users.noreply.github.com>
2026-03-06 09:23:41 +0100

    Change button ID from 'sorpresa' to 'hexa'

--- ejercicios/fondoAleatorio/hexa.js
+++ ejercicios/fondoAleatorio/hexa.js
@@ -1,6 +1,6 @@
 const hexa = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
 
-const boton = document.getElementById('sorpresa')
+const boton = document.getElementById('hexa')
 
 boton.addEventListener('click', function(){
     let color = "#"
